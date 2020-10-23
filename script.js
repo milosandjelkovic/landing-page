aa19b6915996110282aa9eeb14ecb2d6ee63cf85
milosandjelkovic <midje83@gmail.com>
2020-10-24 01:07:07 +0200

    old script.js deleted

--- script.js
+++
@@ -1,9 +0,0 @@
-var navBarItems = document.getElementsByClassName('nav-link');
-for (var i = 0; i < navBarItems.length; i++) {
-	navBarItems[i].addEventListener('click', 
-	function(){
-		var active = document.getElementsByClassName('default');
-		active[0].className = active[0].className.replace(' default', '');
-		this.className += ' default';
-	});
-}
